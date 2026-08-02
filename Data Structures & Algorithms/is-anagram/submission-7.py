7308fe3116dfc99a935944043c535f01add16021
George Tay <georgetay71@gmail.com>
2026-08-02 23:32:09 +0800

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+class Solution:
+    def char2idx(self, char: str) -> int:
+        return ord(char) - ord("a")
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        char_s, char_t = [0 for i in range(26)], [0 for i in range(26)]
+        for char in s:
+            char_s[self.char2idx(char)] += 1
+        for char in t:
+            char_t[self.char2idx(char)] += 1
+        
+        for i in range(26):
+            if char_s[i] != char_t[i]:
+                return False
+        
+        return True
